921b09cc3c5d96a2b0c1a12956e25c098441d9c4
guilhermeqmaia <guilherme.maia@warren.com.br>
2022-09-12 11:00:49 -0300

    fix: device preview and bottom padding

--- README.md
+++ README.md
@@ -3,9 +3,7 @@
 -- Card 2
 
 - Estruturação em pastas para posteriormente criar um usecase para consumir API usando a entidade criada, que será alterada para se adaptar aos dados.
-
+- Saldo ajustado de acordo com o balanço de cada cripto
 - Uso do package intl para formatação de moedas em arquivo da pasta utils.
-
 - Utilização de gerenciador de estado(hooks_riverpod) para facilitar utilização de dados ao realizar a componentização futuramente.
-
 - Uso do package device_preview para testes de responsividade.
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'app_widget.dart';
 
+// void main() {
+//   runApp(DevicePreview(
+//       enabled: !kReleaseMode,
+//       builder: (context) {
+//         return const ProviderScope(child: MyApp());
+//       }));
+// }
+
 void main() {
-  runApp(DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) {
-        return const ProviderScope(child: MyApp());
-      }));
+  runApp(const ProviderScope(child: MyApp()));
 }
