e8e491f70bbf72636c708b9e67e2836b61a41c01
guilhermeqmaia <guilherme.maia@warren.com.br>
2022-09-23 07:57:53 -0300

    refactor: removing unnecessary future builder at details screen

--- lib/details/widgets/body_details_screen.dart
+++ lib/details/widgets/body_details_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../controller/days_provider.dart';
 import '../controller/historic_data_provider.dart';
-import '../model/prices_view_data.dart';
 import 'info_row_details.dart';
 import '../../portfolio/model/crypto_view_data.dart';
 import '../../shared/utils/currency_formater.dart';
@@ -27,83 +26,70 @@ class BodyDetailsScreen extends HookConsumerWidget {
 
     return cryptoData.when(
       data: (data) {
-        return FutureBuilder(
-          future: ref.watch(historicDataProvider(coin.id).future),
-          builder: (context, AsyncSnapshot<PricesViewData?> snapshot) {
-            if (snapshot.hasData) {
-              double variation = (snapshot.data!.prices.reversed.first.last /
-                          snapshot.data!.prices.reversed.elementAt(day).last -
-                      1) *
-                  100;
-              return SingleChildScrollView(
+        double variation =
+            (data.prices.last.last / data.prices.reversed.elementAt(day).last -
+                    1) *
+                100;
+        return SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TopPageContainer(
+                model: coin,
+              ),
+              GraphDetails(
+                historyCoinData: List<FlSpot>.from(
+                  data.prices.reversed.map(
+                    (crypto) {
+                      return FlSpot(
+                        crypto[0].toDouble(),
+                        crypto[1].toDouble(),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              const ChangeDaysButtons(),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 30,
+                  top: 20,
+                  bottom: 20,
+                ),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    TopPageContainer(
-                      model: coin,
-                    ),
-                    GraphDetails(
-                      historyCoinData: List<FlSpot>.from(
-                        snapshot.data!.prices.reversed.map(
-                          (crypto) {
-                            return FlSpot(
-                              crypto[0].toDouble(),
-                              crypto[1].toDouble(),
-                            );
-                          },
-                        ),
+                    const Divider(thickness: 1),
+                    InfoRowDetails(
+                      label: 'Preço ${day}D',
+                      text: currencyFormatter.format(
+                        data.prices.reversed.elementAt(day).last,
                       ),
                     ),
-                    const ChangeDaysButtons(),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 30,
-                        top: 20,
-                        bottom: 20,
-                      ),
-                      child: Column(
-                        children: [
-                          const Divider(thickness: 1),
-                          InfoRowDetails(
-                            label: 'Preço ${day}D',
-                            text: currencyFormatter.format(
-                              snapshot.data!.prices.reversed
-                                  .elementAt(day)
-                                  .last,
-                            ),
-                          ),
-                          const Divider(thickness: 1),
-                          InfoRowDetails(
-                            label: 'Variação ${day}D',
-                            text:
-                                '${variation > 0 ? '+' : ''} ${variation.toStringAsFixed(2)}%',
-                            color: variation > 0 ? Colors.green : Colors.red,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          const Divider(thickness: 1),
-                          InfoRowDetails(
-                            label: 'Quantidade',
-                            text: '0.5 ${coin.symbol.toUpperCase()}',
-                          ),
-                          const Divider(thickness: 1),
-                          InfoRowDetails(
-                            label: 'Valor',
-                            text: currencyFormatter
-                                .format(coin.currentPrice * 0.5),
-                          ),
-                        ],
-                      ),
+                    const Divider(thickness: 1),
+                    InfoRowDetails(
+                      label: 'Variação ${day}D',
+                      text:
+                          '${variation > 0 ? '+' : ''} ${variation.toStringAsFixed(2)}%',
+                      color: variation > 0 ? Colors.green : Colors.red,
+                      fontWeight: FontWeight.w500,
                     ),
-                    WarrenButton(onPressed: () {}),
+                    const Divider(thickness: 1),
+                    InfoRowDetails(
+                      label: 'Quantidade',
+                      text: '0.5 ${coin.symbol.toUpperCase()}',
+                    ),
+                    const Divider(thickness: 1),
+                    InfoRowDetails(
+                      label: 'Valor',
+                      text: currencyFormatter.format(coin.currentPrice * 0.5),
+                    ),
                   ],
                 ),
-              );
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
+              ),
+              WarrenButton(onPressed: () {}),
+            ],
+          ),
         );
       },
       error: (e, r) {
