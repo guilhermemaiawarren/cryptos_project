8cce86dcd12e65548432c436e307381a01a9ab36
guilhermeqmaia <guilherme.maia@warren.com.br>
2022-09-23 10:59:32 -0300

    feat: using serializable to getAssets on portfolio screen

--- lib/shared/api/models/cryptos_response.dart
+++ lib/shared/api/models/cryptos_response.dart
@@ -1,6 +1,8 @@
+import 'package:json_annotation/json_annotation.dart';
 
+part 'cryptos_response.g.dart';
 
-
+@JsonSerializable()
 class CryptosResponse {
   String id;
   String symbol;
@@ -17,29 +19,8 @@ class CryptosResponse {
     this.variation,
   );
 
+  factory CryptosResponse.fromJson(Map<String, dynamic> json) =>
+      _$CryptosResponseFromJson(json);
 
-
-  Map<String, dynamic> toMap() {
-    final result = <String, dynamic>{};
-
-    result.addAll({'id': id});
-    result.addAll({'symbol': symbol});
-    result.addAll({'name': name});
-    result.addAll({'image': image});
-    result.addAll({'current_price': currentPrice});
-    result.addAll({'price_change_percentage_24h': variation});
-
-    return result;
-  }
-
-  factory CryptosResponse.fromMap(Map<String, dynamic> map) {
-    return CryptosResponse(
-      map['id'] ?? '',
-      map['symbol'] ?? '',
-      map['name'] ?? '',
-      map['image'] ?? '',
-      map['current_price']?.toDouble() ?? 0.0,
-      map['price_change_percentage_24h']?.toDouble() ?? 0.0,
-    );
-  }
+  Map<String, dynamic> toJson() => _$CryptosResponseToJson(this);
 }
