a7792db32f25b84f4165e6f6a22d17c3981c405f
guilhermeqmaia <guilherme.maia@warren.com.br>
2022-09-26 08:56:55 -0300

    feat: FormValidator

--- lib/conversion/view/conversion_screen.dart
+++ lib/conversion/view/conversion_screen.dart
@@ -11,8 +11,8 @@ import 'package:projeto_criptos/shared/templates/model_app_bar.dart';
 
 import '../../shared/utils/decimal_parse.dart';
 
-class ConversionScreen extends HookConsumerWidget {
-  ConversionScreen({
+class ConversionScreen extends StatefulHookConsumerWidget {
+  const ConversionScreen({
     Key? key,
     required this.coinAmmount,
     required this.asset,
@@ -20,235 +20,251 @@ class ConversionScreen extends HookConsumerWidget {
   static const route = '/conversion';
   final Decimal coinAmmount;
   final CryptoEntity asset;
-  final TextEditingController convertController = TextEditingController();
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConversionScreen> createState() => _$ConversionScreenState();
+}
+
+class _$ConversionScreenState extends ConsumerState<ConversionScreen> {
+  final TextEditingController convertController = TextEditingController();
+  bool validate = false;
+
+  @override
+  Widget build(BuildContext context) {
     final cryptos = ref.watch(cryptosProvider);
     var cryptoConverted = ref.watch(convertedCrypto.state).state;
     final formKey = GlobalKey<FormState>();
     Decimal convertInReals = dp('0.0');
-    return cryptos.when(data: (data) {
-      cryptoConverted = data[1].id == asset.id ? data[0] : data[1];
-      return Scaffold(
-        appBar: const ModelAppBar(text: 'Converter'),
-        body: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 56,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                ),
-                color: Colors.grey.shade200,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Saldo Disponível',
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                        fontFamily: AppAssets.montSerrat,
-                        fontSize: 16,
+    return cryptos.when(
+      data: (data) {
+        cryptoConverted = data[1].id == widget.asset.id ? data[0] : data[1];
+        return Scaffold(
+          appBar: const ModelAppBar(text: 'Converter'),
+          body: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 56,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                  ),
+                  color: Colors.grey.shade200,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Saldo Disponível',
+                        style: TextStyle(
+                          color: Colors.grey.shade600,
+                          fontFamily: AppAssets.montSerrat,
+                          fontSize: 16,
+                        ),
                       ),
-                    ),
-                    Text(
-                      '${coinAmmount.toStringAsFixed(4)} ${asset.symbol.toUpperCase()}',
-                      style: TextStyle(
-                        fontFamily: AppAssets.montSerrat,
-                        fontSize: 18,
+                      Text(
+                        '${widget.coinAmmount.toStringAsFixed(4)} ${widget.asset.symbol.toUpperCase()}',
+                        style: TextStyle(
+                          fontFamily: AppAssets.montSerrat,
+                          fontSize: 18,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 20,
-                  horizontal: 30,
-                ),
-                child: Text(
-                  'Quanto você gostaria de converter?',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontFamily: AppAssets.montSerrat,
-                    fontWeight: FontWeight.bold,
+                    ],
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  MaterialButton(
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(color: Colors.grey.shade300),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    onPressed: () {},
-                    child: Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 9,
-                          backgroundColor: Colors.transparent,
-                          backgroundImage: Image.network(asset.image).image,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            left: 5,
-                          ),
-                          child: Text(asset.symbol.toUpperCase()),
-                        ),
-                        const Icon(
-                          Icons.keyboard_arrow_down,
-                          size: 20,
-                          color: Colors.grey,
-                        ),
-                      ],
-                    ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 30,
                   ),
-                  Icon(
-                    Icons.sync_alt,
-                    color: AppAssets.magenta,
-                    size: 25,
-                  ),
-                  MaterialButton(
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(color: Colors.grey.shade300),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    onPressed: () {},
-                    child: Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 9,
-                          backgroundColor: Colors.transparent,
-                          backgroundImage:
-                              Image.network(cryptoConverted.image).image,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            left: 5,
-                          ),
-                          child: Text(cryptoConverted.symbol.toUpperCase()),
-                        ),
-                        const Icon(
-                          Icons.keyboard_arrow_down,
-                          size: 20,
-                          color: Colors.grey,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 10,
-                ),
-                child: TextFormField(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  controller: convertController,
-                  decoration: InputDecoration(
-                    hintText: '${asset.symbol.toUpperCase()} 0,00',
-                    hintStyle: const TextStyle(
-                      color: Colors.grey,
+                  child: Text(
+                    'Quanto você gostaria de converter?',
+                    style: TextStyle(
                       fontSize: 25,
+                      fontFamily: AppAssets.montSerrat,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  keyboardType: TextInputType.number,
-                  onChanged: (value) {
-                    if (dp(convertController.text) < coinAmmount) {
-                      convertInReals =
-                          dp(convertController.text) * asset.currentPrice;
-                    } else {
-                      convertInReals = dp('0.0');
-                    }
-                  },
-                  validator: (value) {
-                    if (value == null || value == '') {
-                      return 'Valor não pode ser nulo';
-                    } else if (dp(convertController.text) > coinAmmount) {
-                      return 'Valor não pode ser maior do que o disponível na carteira';
-                    }
-                    return null;
-                  },
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 10,
-                ),
-                child: Text(
-                  'R\$ $convertInReals',
-                  style: const TextStyle(
-                    color: Colors.grey,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Container(
-                      width: double.maxFinite,
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 20,
-                        horizontal: 30,
+                    MaterialButton(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(30),
                       ),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          top: BorderSide(color: Colors.grey.shade300),
-                        ),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      onPressed: () {},
+                      child: Row(
                         children: [
-                          Text(
-                            'Total estimado',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: AppAssets.montSerrat,
-                            ),
+                          CircleAvatar(
+                            radius: 9,
+                            backgroundColor: Colors.transparent,
+                            backgroundImage:
+                                Image.network(widget.asset.image).image,
                           ),
-                          const SizedBox(height: 8),
-                          Text(
-                            '0,0000 ${cryptoConverted.symbol.toUpperCase()}',
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w500,
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              left: 5,
                             ),
+                            child: Text(widget.asset.symbol.toUpperCase()),
+                          ),
+                          const Icon(
+                            Icons.keyboard_arrow_down,
+                            size: 20,
+                            color: Colors.grey,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Icon(
+                      Icons.sync_alt,
+                      color: AppAssets.magenta,
+                      size: 25,
+                    ),
+                    MaterialButton(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 9,
+                            backgroundColor: Colors.transparent,
+                            backgroundImage:
+                                Image.network(cryptoConverted.image).image,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              left: 5,
+                            ),
+                            child: Text(cryptoConverted.symbol.toUpperCase()),
+                          ),
+                          const Icon(
+                            Icons.keyboard_arrow_down,
+                            size: 20,
+                            color: Colors.grey,
                           ),
                         ],
                       ),
                     ),
                   ],
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 10,
+                  ),
+                  child: TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    controller: convertController,
+                    decoration: InputDecoration(
+                      hintText: '${widget.asset.symbol.toUpperCase()} 0.00',
+                      hintStyle: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 25,
+                      ),
+                    ),
+                    keyboardType:
+                        const TextInputType.numberWithOptions(decimal: true),
+                    onFieldSubmitted: (value) {
+                      if (formKey.currentState!.validate()) {
+                        validate = true;
+                      } else {
+                        validate = false;
+                      }
+                      setState(() {});
+                    },
+                    validator: (value) {
+                      if (value == null || value == '') {
+                        return 'Valor não pode ser igual a 0';
+                      } else if (double.parse(convertController.text
+                              .replaceAll(RegExp(r'[^\w\s]+'), '.')) >
+                          widget.coinAmmount.toDouble()) {
+                        return 'Valor não pode ser maior do que o disponível na carteira';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 10,
+                  ),
+                  child: Text(
+                    'R\$ $convertInReals',
+                    style: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        width: double.maxFinite,
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 30,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            top: BorderSide(color: Colors.grey.shade300),
+                          ),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Total estimado',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: AppAssets.montSerrat,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              '0,0000 ${cryptoConverted.symbol.toUpperCase()}',
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: AppAssets.magenta,
-          onPressed: () {},
-          child: const Icon(
-            Icons.keyboard_arrow_right,
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: validate ? AppAssets.magenta : Colors.grey,
+            onPressed: validate ? () {} : null,
+            child: const Icon(
+              Icons.keyboard_arrow_right,
+            ),
           ),
-        ),
-      );
-    }, error: (e, s) {
-      return ErrorBody(
-        onError: () {
-          ref.refresh(cryptosProvider);
-        },
-      );
-    }, loading: () {
-      return const LoadingBody();
-    });
+        );
+      },
+      error: (e, s) {
+        return ErrorBody(
+          onError: () {
+            ref.refresh(cryptosProvider);
+          },
+        );
+      },
+      loading: () {
+        return const LoadingBody();
+      },
+    );
   }
 }
