34fe0c04754a229910e0c493abccb7c4825fac63
guilhermeqmaia <guilherme.maia@warren.com.br>
2022-09-27 09:56:22 -0300

    fix: initstate of price provider in details

--- lib/details/view/details_screen.dart
+++ lib/details/view/details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:projeto_criptos/details/controller/get_price_provider.dart';
 import '../../shared/common_model/crypto.dart';
 import '../../shared/utils/arguments/to_details_arguments.dart';
 
@@ -23,6 +24,7 @@ class DetailsScreen extends HookConsumerWidget {
     final args = ModalRoute.of(context)!.settings.arguments as ToDetailsArguments;
     Future.delayed(Duration.zero, () {
       ref.read(daysProvider.state).state = 5;
+      ref.read(getPriceProvider.state).state = '';
     });
     return Scaffold(
       appBar: const ModelAppBar(
