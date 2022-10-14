a084391c3a30a11cfb2f949ea6359fc072e8826e
Guilherme Maia <113136109+guilhermemaiawarren@users.noreply.github.com>
2022-10-14 10:29:32 -0300

    refactor: change branch name

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -6,7 +6,6 @@ import 'route_controller.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
